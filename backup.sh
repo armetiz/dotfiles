1fac651328660f43172db8e32f2f6dbeb5d0fd7b
Thomas Tourlourat <thomas@tourlourat.com>
2015-09-16 10:36:23 +0200

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,3 @@
+rsync -vlrt --stats --progress --delete /Volumes/photos /Volumes/Backup
+rsync -vlrt --stats --progress --delete /Volumes/thomas /Volumes/Backup
+rsync -vlrt --stats --progress --delete /Volumes/commun /Volumes/Backup
