d7e3434d3e7af8b59e03e4f908a054e5e74c2451
Thomas Tourlourat <thomas@tourlourat.com>
2016-05-06 15:37:57 +0200

    add ansible

--- deps.sh
+++ deps.sh
@@ -4,6 +4,6 @@ brew tap homebrew/dupes
 brew tap homebrew/versions
 brew tap homebrew/homebrew-php
 
-brew install php70 php70-intl php70-xdebug watch wget mtr git git-crypt lorem icu4c iperf
+brew install php70 php70-intl php70-xdebug watch wget mtr git git-crypt lorem icu4c iperf ansible
 
 brew cask install iterm2 vlc alfred virtualbox vagrant spectacle sublime-text slack skype phpstorm limechat google-chrome firefox filezilla 1password vnc-viewer
