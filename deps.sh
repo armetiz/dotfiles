bcb539789c818ab860b5c30f18097019a98ffb23
Thomas Tourlourat <thomas@tourlourat.com>
2016-05-06 12:44:12 +0200

    update

--- deps.sh
+++ deps.sh
@@ -4,6 +4,6 @@ brew tap homebrew/dupes
 brew tap homebrew/versions
 brew tap homebrew/homebrew-php
 
-brew install php70 watch wget mtr git git-crypt lorem uc4c iperf
+brew install php70 watch wget mtr git git-crypt lorem icu4c iperf
 
 brew cask install iterm2 vlc alfred virtualbox vagrant spectacle sublime slack skype phpstorm limechat chrome firefox filezilla
