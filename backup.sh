554ca6e337c37d3b08c38bff6c2a281c704fc113
Thomas Tourlourat <thomas@tourlourat.com>
2017-11-01 18:00:05 +0100

    do not backup workspace... disk too little

--- backup.sh
+++ backup.sh
@@ -2,4 +2,3 @@ rsync -vlrt --stats --progress --delete /Volumes/photos /Volumes/Backup
 rsync -vlrt --stats --progress --delete /Volumes/thomas /Volumes/Backup
 rsync -vlrt --stats --progress --delete /Volumes/commun /Volumes/Backup
 rsync -vlrt --stats --progress --delete ~/Library/Application\ Support/1Password\ 4/Backups /Volumes/Backup/1Password
-rsync -vlrt --stats --progress --delete /Users/thomastourlourat/Workspace /Volumes/Backup
