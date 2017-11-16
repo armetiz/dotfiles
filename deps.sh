70dd709fcb3a0b55416b8f002d1a586c8b1d8ac7
Thomas Tourlourat <thomas@tourlourat.com>
2017-11-16 15:31:56 +0100

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -4,6 +4,8 @@ brew tap homebrew/dupes
 brew tap homebrew/versions
 brew tap homebrew/homebrew-php
 
-brew install php70 php70-intl php70-xdebug watch wget mtr git git-crypt lorem icu4c iperf ansible composer htop
+brew install php71 php71-intl php71-xdebug watch wget mtr git git-crypt lorem icu4c iperf ansible composer htop
 
-brew cask install iterm2 vlc alfred virtualbox vagrant spectacle sublime-text slack skype phpstorm limechat google-chrome firefox filezilla 1password vnc-viewer disk-inventory-x sequel-pro unrarx node medis
+brew cask install iterm2 vlc alfred vagrant spectacle sublime-text google-chrome firefox disk-inventory-x sequel-pro node
+brew cask install skype 1password phpstorm slack
+brew cask install medis virtualbox virtualbox-extension-pack unrarx
