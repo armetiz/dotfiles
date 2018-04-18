8aa9cc6d9120d5d7e3f885c09a4132cefe2109dd
Thomas Tourlourat <thomas@tourlourat.com>
2018-04-18 14:41:37 +0200

    Add vagrant plugin: vagrant-vbguest

--- deps.sh
+++ deps.sh
@@ -9,3 +9,5 @@ brew install php71 php71-intl php71-xdebug watch wget mtr git git-crypt lorem ic
 brew cask install iterm2 vlc alfred vagrant spectacle sublime-text google-chrome firefox disk-inventory-x sequel-pro node
 brew cask install skype 1password phpstorm slack
 brew cask install medis virtualbox virtualbox-extension-pack unrarx
+
+vagrant plugin install vagrant-vbguest
