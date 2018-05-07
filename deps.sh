0728285ac65d0af3953f91b7d6ae3a1b929451eb
Thomas Tourlourat <thomas@tourlourat.com>
2018-05-07 13:29:37 +0200

    wip

--- deps.sh
+++ deps.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-brew tap homebrew/dupes
-brew tap homebrew/versions
+brew upgrade
 brew tap homebrew/homebrew-php
 
-brew install php71 php71-intl php71-xdebug watch wget mtr git git-crypt lorem icu4c iperf ansible composer htop
+brew install php71 php71-intl php71-xdebug watch wget mtr git git-crypt lorem icu4c iperf ansible composer htop node httpie
 
-brew cask install iterm2 vlc alfred vagrant spectacle sublime-text google-chrome firefox disk-inventory-x sequel-pro node
+brew cask install iterm2 vlc alfred vagrant spectacle sublime-text google-chrome firefox disk-inventory-x sequel-pro
 brew cask install skype 1password phpstorm slack
-brew cask install medis virtualbox virtualbox-extension-pack unrarx
+brew cask install virtualbox virtualbox-extension-pack
 
 vagrant plugin install vagrant-vbguest
