e836bc612da98aa4a45e4d2f99d236aa3223c7b1
Thomas Tourlourat <thomas@tourlourat.com>
2018-09-17 23:28:39 -0700

    comment php

--- deps.sh
+++ deps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 brew upgrade
-brew tap homebrew/homebrew-php
-
-brew install php71 php71-intl php71-xdebug watch wget mtr git git-crypt lorem icu4c iperf ansible composer htop node httpie
+# brew tap homebrew/homebrew-php
+# brew install php71 php71-intl php71-xdebug composer
+brew install watch wget mtr git lorem icu4c iperf ansible htop node httpie
 
 brew cask install iterm2 vlc alfred vagrant spectacle sublime-text google-chrome firefox disk-inventory-x sequel-pro
 brew cask install skype 1password phpstorm slack
