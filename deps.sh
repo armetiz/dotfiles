d6fb4d64d8a58bc16fcf8b0218e0f262841bde97
Thomas Tourlourat <thomas@tourlourat.com>
2018-10-13 09:15:00 +0200

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -5,7 +5,7 @@ brew upgrade
 # brew install php71 php71-intl php71-xdebug composer
 brew install watch wget mtr git lorem icu4c iperf ansible htop node httpie
 
-brew cask install iterm2 vlc alfred vagrant spectacle sublime-text google-chrome firefox disk-inventory-x sequel-pro
+brew cask install iterm2 iina alfred vagrant spectacle sublime-text google-chrome firefox disk-inventory-x sequel-pro
 brew cask install skype 1password phpstorm slack
 brew cask install virtualbox virtualbox-extension-pack
 brew cask install tunnelbear
