6a4f576e539896d5cd9b11f2441b79476c598c13
Thomas Tourlourat <thomas@tourlourat.com>
2023-03-01 08:43:45 +0100

    cc

--- deps.sh
+++ deps.sh
@@ -5,7 +5,7 @@ brew upgrade
 # brew install php71 php71-intl php71-xdebug composer
 brew install watch wget git lorem icu4c iperf htop node
 
-brew cask install iterm2 alfred vagrant spectacle sublime-text brave-browser disk-inventory-x sequel-ace
+brew cask install iterm2 alfred vagrant spectacle sublime-text brave-browser firefox sequel-ace
 brew cask install 1password phpstorm slack
 brew cask install virtualbox virtualbox-extension-pack
 brew cask install tunnelbear grandperspective
